9e675b2a95a6b763117901cacbf3d6755a65c916
Adam Nagel <adam.nagel+git@gmail.com>
2015-11-12 14:29:36 -0600

    wip

--- openmdao-problem.py
+++ openmdao-problem.py
@@ -8,6 +8,8 @@ from openmdao.drivers.scipy_optimizer import ScipyOptimizer
 from openmdao.api import ExternalCode
 from openmdao.recorders.dump_recorder import DumpRecorder
 from ommodelwrapper.ommodelwrapper import OMModelWrapper
+from openmdao.drivers.uniform_driver import UniformDriver
+from openmdao.recorders.csv_recorder import CsvRecorder
 from collections import Counter
 import json
 import sys
@@ -16,6 +18,18 @@ import string
 import random
 import copy
 import six
+import numpy as np
+
+
+class TakeLast(Component):
+    def __init__(self):
+        super(TakeLast, self).__init__()
+        self.add_param('input', val=0.1)
+        self.add_output('output', val=0.1, pass_by_obj=True)
+
+    def solve_nonlinear(self, params, unknowns, resids):
+        unknowns['output'] = params['input'][-1]
+
 
 if __name__ == "__main__":
     top = Problem()
@@ -23,18 +37,27 @@ if __name__ == "__main__":
     root = top.root = Group()
 
     root.add('thing', IndepVarComp('simtime', val="15.0"))
-    root.add('modelica', OMModelWrapper( 'WindPowerPlants.Examples.GenericPlantRayleigh', '/Users/adam/repo/WindPowerPlants/WindPowerPlants/package.mo'))
-    # root.add('modelica', OMModelWrapper( 'WindPowerPlants/Examples/GenericPlantRayleigh', '/Users/adam/repo/WindPowerPlants/WindPowerPlants/'))
+    root.add('p1', IndepVarComp('nz', val=1.0))
+    root.add('p2', IndepVarComp('nn', val=1.0))
+    root.add('modelica',
+             OMModelWrapper('WindPowerPlants.Examples.GenericPlantRayleigh',
+                            '/Users/adam/repo/WindPowerPlants/WindPowerPlants/package.mo'))
+    root.add('tl', TakeLast())
 
-    # top.driver = ScipyOptimizer()
-    # top.driver.options['optimizer'] = 'SLSQP'
-    # top.driver.add_desvar('p1.Voltage', low=1.0, high=10.0)
-    # top.driver.add_desvar('p2.Resistance', low=5.0, high=25.0)
-    # top.driver.add_objective('tb.Current')
+    top.driver = UniformDriver(1)
+    top.driver.add_desvar('p2.nn', low=1.0, high=10.0)
+    top.driver.add_desvar('p1.nz', low=5.0, high=25.0)
+    top.driver.add_objective('modelica.y_testvariable')
 
     root.connect('thing.simtime', 'modelica.stopTime')
+    root.connect('p1.nz', 'modelica.nz')
+    root.connect('p2.nn', 'modelica.nn')
+    root.connect('modelica.y_testvariable', 'tl.input')
 
-    recorder = DumpRecorder()
+    # root.connect('thing.simtime', 'modelica.stopTime')
+    # root.connect('thing2.nz', 'modelica.nz')
+
+    recorder = CsvRecorder('dump.csv')
     recorder.options['record_params'] = True
     recorder.options['record_metadata'] = True
     recorder.options['record_resids'] = True
@@ -42,5 +65,8 @@ if __name__ == "__main__":
 
     top.setup()
     top.run()
+
+    print ('modelica.y_testvariable', top['modelica.y_testvariable'])
+    print ('tl.output', top['tl.output'])
 
     top.driver.recorders[0].close()
