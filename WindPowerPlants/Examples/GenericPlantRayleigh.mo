0f5e92e3a23dfbefbe3909ed73dc3f598343e3b7
Adam Nagel <adam.nagel+git@gmail.com>
2015-11-13 15:23:53 -0600

    first experiment

--- WindPowerPlants/Examples/GenericPlantRayleigh.mo
+++ WindPowerPlants/Examples/GenericPlantRayleigh.mo
@@ -9,7 +9,7 @@ model GenericPlantRayleigh "Generic wind power plant simulation with statistical
   Real peakPowerOutput = max(peakPowerOutput, abs(plant.power));
   Modelica.Blocks.Continuous.Integrator energyIntegrator(k = 1) annotation(Placement(transformation(origin = {60, 20}, extent = {{-10, -10}, {10, 10}})));
   Plants.GenericVariableSpeed plant(rho = rho, JTurbine = jturbine, ratio = ratio) annotation(Placement(transformation(extent = {{10, -10}, {30, 10}})));
-  WindPowerPlants.WindSources.Rayleigh windSource(n = 25, vMax = 25, vMean = 6.07, T = 2678400) annotation(Placement(transformation(origin = {-40, 0}, extent = {{-10, -10}, {10, 10}})));
+  WindPowerPlants.WindSources.Rayleigh windSource(n = 25, vMax = 25, vMean = 6.07, T = 100) annotation(Placement(transformation(origin = {-40, 0}, extent = {{-10, -10}, {10, 10}})));
   Blocks.SpeedAdaptor speedadaptor1(hin = 50, hout = 105, roughness = 0.1) annotation(Placement(transformation(origin = {-10, 0}, extent = {{-10, -10}, {10, 10}})));
   Blocks.LogTerminal logTerminal(preString = "Total energy = ", postString = " kWh", gain = 1 / 3.6E6) annotation(Placement(transformation(extent = {{80, 10}, {100, 30}})));
 equation
--- openmdao-problem.py
+++ openmdao-problem.py
@@ -7,9 +7,10 @@ from openmdao.core.driver import Driver
 from openmdao.drivers.scipy_optimizer import ScipyOptimizer
 from openmdao.api import ExternalCode
 from openmdao.recorders.dump_recorder import DumpRecorder
+from openmdao.recorders.base_recorder import BaseRecorder
 from ommodelwrapper.ommodelwrapper import OMModelWrapper
 from openmdao.drivers.uniform_driver import UniformDriver
-from openmdao.recorders.csv_recorder import CsvRecorder
+from openmdao.util.record_util import format_iteration_coordinate
 from collections import Counter
 import json
 import sys
@@ -18,7 +19,44 @@ import string
 import random
 import copy
 import six
+import csv
 import numpy as np
+
+
+class CsvRecorder(BaseRecorder):
+
+    def __init__(self, out=sys.stdout):
+        super(CsvRecorder, self).__init__()
+
+        self._wrote_header = False
+        self._parallel = False
+
+        if out != sys.stdout:
+            self.out = out
+        self.writer = csv.writer(out)
+
+    def startup(self, group):
+        super(CsvRecorder, self).startup(group)
+
+    def record_iteration(self, params, unknowns, resids, metadata):
+        filtered_unknowns = [unknown for unknown in unknowns if unknown.startswith('tl_')]
+        filtered_params = [param for param in params if param.startswith('modelica')]
+
+        if self._wrote_header is False:
+            self.writer.writerow(filtered_params + filtered_unknowns)
+            self._wrote_header = True
+
+        def munge(val):
+            if isinstance(val, np.ndarray):
+                return ",".join(map(str, val))
+            return str(val)
+        self.writer.writerow([munge(value['val']) for key, value in params.iteritems() if key in filtered_params] + [munge(value['val']) for key, value in unknowns.iteritems() if key in filtered_unknowns])
+
+        if self.out:
+            self.out.flush()
+
+    def record_metadata(self, group):
+        pass
 
 
 class TakeLast(Component):
@@ -36,13 +74,13 @@ if __name__ == "__main__":
 
     root = top.root = Group()
 
-    root.add('simprop', IndepVarComp('simtime', val="15.0"))
+    root.add('simprop', IndepVarComp('simtime', val="100"))
     root.add('p1', IndepVarComp('rho', val=1.0))
     root.add('p2', IndepVarComp('jturbine', val=1.0))
     root.add('p3', IndepVarComp('ratio', val=1.0))
     root.add('modelica',
              OMModelWrapper('WindPowerPlants.Examples.GenericPlantRayleigh',
-                            '/Users/adam/repos/WindPowerPlants/WindPowerPlants/package.mo'))
+                            '/Users/adam/repo/WindPowerPlants/WindPowerPlants/package.mo'))
     root.add('tl_peakPowerOutput', TakeLast())
     root.add('tl_integratedEnergy', TakeLast())
 
@@ -63,7 +101,7 @@ if __name__ == "__main__":
     # root.connect('thing.simtime', 'modelica.stopTime')
     # root.connect('thing2.nz', 'modelica.nz')
 
-    recorder = CsvRecorder('dump.csv')
+    recorder = CsvRecorder(open('dump.csv', 'w'))
     recorder.options['record_params'] = False
     recorder.options['record_metadata'] = False
     recorder.options['record_resids'] = False
